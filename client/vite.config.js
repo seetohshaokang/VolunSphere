67f801908d907d4f3ad58d5eac3068f6ed1bbb86
shao <shaokang8069@gmail.com>
2025-03-28 22:37:57 +0800

    frontend -- installed shadcn ui and components

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,13 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+import path from "path";
+
 export default defineConfig({
 	plugins: [react()],
-	base: "/",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
